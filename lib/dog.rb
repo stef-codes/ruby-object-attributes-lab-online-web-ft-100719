faaf5acc102c36af1711a24752e14fb44a3441f2
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-16 16:26:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog
-#   def initialize(name)
-#     @name = name
-#   end
+  def initialize(name)
+    @name = name
+  end
   
-# def name
-#     @name
-#   end
+def name
+    @name
+  end
   
   def name=(name)
     @name = name
   end
 end
 
-fido = Dog.new("Fido")
-fido.name
+# fido = Dog.new("Fido")
+# fido.name
