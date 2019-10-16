3560a060f223ec2f979a614fad29395af696a434
Stefanie Davis <davisstefanie2010@gmail.com>
2019-10-16 16:13:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,10 @@
 class Dog
-#   def initialize(name)
-#     @name = name
-#   end
-  
-# def name
-#     @name
-#   end
- 
-  def name=(new_name)
-    @name = new_name
+  def initialize(name)
+    @name = name
   end
   
+def name
+    @name
+  end
 end
+
